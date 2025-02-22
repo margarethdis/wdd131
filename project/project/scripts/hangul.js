02bcaa5f5cf581af82cf248643ad33a3e20406f2
MargarethRamos <104832344+margarethdis@users.noreply.github.com>
2025-02-21 22:10:50 -0600

    Delete project/project directory

--- project/project/scripts/hangul.js
+++
@@ -1,8 +0,0 @@
-// Selecciona el botón y el menú
-const menuToggle = document.getElementById("menu-toggle");
-const navMenu = document.querySelector("nav ul");
-
-// Agrega un evento de clic para alternar la clase 'active'
-menuToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-});
