1a450f6505d9e3c18bf9f816a4429b0974563016
margarethdis <margarethdani2021@gmail.com>
2025-08-02 14:05:28 -0600

    korea

---
+++ scripts/place.js
@@ -0,0 +1,5 @@
+// Mostrar año actual
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Mostrar fecha de última modificación
+document.getElementById('lastModified').textContent = `Last modified: ${document.lastModified}`;
