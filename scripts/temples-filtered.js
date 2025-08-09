a0be6f4cacee78ba2edb7ebf5c76c6ca5d8886b2
margarethdis <margarethdani2021@gmail.com>
2025-08-09 14:18:04 -0600

    ,

--- scripts/temples-filtered.js
+++ scripts/temples-filtered.js
@@ -5,28 +5,28 @@ const temples = [
         location: "Salt Lake City, Utah",
         dedicated: "6 April 1893",
         area: 253015,
-        imageUrl: "https://churchofjesuschrist.org/img/salt-lake-temple.jpg"
+        imageUrl: "https://www.churchofjesuschrist.org/imgs/cd4d22a965af18a50f8589bb1b917859d5935209/full/!1600%2C1071/0/default"
     },
     {
         templeName: "Laie Hawaii Temple",
         location: "Laie, Hawaii",
         dedicated: "27 November 1919",
         area: 42500,
-        imageUrl: "https://churchofjesuschrist.org/img/laie-hawaii-temple.jpg"
+        imageUrl: "https://www.churchofjesuschrist.org/imgs/809f567ccf240d2f1c8e457e8c81fbd94ef96759/full/500%2C/0/default"
     },
     {
         templeName: "Kirtland Temple",
         location: "Kirtland, Ohio",
         dedicated: "27 March 1836",
         area: 11000,
-        imageUrl: "https://churchofjesuschrist.org/img/kirtland-temple.jpg"
+        imageUrl: "https://www.churchofjesuschrist.org/imgs/cpgqik2ds9fs8ktmxxzoruxy5863bwhtycp1vtvt/pct:0,7.8,100,84.5/!1000%2C563/0/default"
     },
     {
         templeName: "Rome Italy Temple",
         location: "Rome, Italy",
         dedicated: "10 March 2019",
         area: 41010,
-        imageUrl: "https://churchofjesuschrist.org/img/rome-italy-temple.jpg"
+        imageUrl: "https://www.churchofjesuschrist.org/imgs/17e2c70d687fffedfe115197e57fa8f5d1d369bb/full/500%2C/0/default"
     },
     // ---- Templos agregados por ti ----
     {
@@ -34,21 +34,21 @@ const temples = [
         location: "Tegucigalpa, Honduras",
         dedicated: "17 March 2013",
         area: 28500,
-        imageUrl: "https://churchofjesuschrist.org/img/tegucigalpa-honduras-temple.jpg"
+        imageUrl: "https://churchofjesuschrist.org/imgs/5eafb4407decaf72553241072f38e2003df39658/full/500%2C/0/default"
     },
     {
         templeName: "Paris France Temple",
         location: "Le Chesnay, France",
         dedicated: "21 May 2017",
         area: 44000,
-        imageUrl: "https://churchofjesuschrist.org/img/paris-france-temple.jpg"
+        imageUrl: "https://www.churchofjesuschrist.org/imgs/5ec026c4efeaaa19a98e40f0f1b4c6069ae63517/full/500%2C/0/default"
     },
     {
         templeName: "Payson Utah Temple",
         location: "Payson, Utah",
         dedicated: "7 June 2015",
         area: 96630,
-        imageUrl: "https://churchofjesuschrist.org/img/payson-utah-temple.jpg"
+        imageUrl: "https://churchofjesuschrist.org/imgs/7b998062094c11eca393eeeeac1e50df07c8fd34/full/500%2C/0/default"
     }
 ];
 
